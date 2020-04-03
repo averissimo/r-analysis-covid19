8771a26004a18357ec6766b91abc3fa44a71f30f
André Veríssimo <afsverissimo@gmail.com>
2020-04-03 11:45:50 +0200

    uses john hopkins data while EU CDC is lagging

--- R/download.R
+++ R/download.R
@@ -155,6 +155,132 @@ download.pt.data <- function() {
         return()
 }
 
+download.john.hopkins <- function() {
+    jh.data.raw.deaths    <- 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' %>% read_csv()
+    jh.data.raw.confirmed <- 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'%>% read_csv()
+
+    jh.data.deaths <- jh.data.raw.deaths %>%
+        melt(id.vars = c('Province/State', 'Country/Region', 'Lat', 'Long'),
+             variable.name = 'date',
+             value.name = 'cases') %>%
+        tibble() %>%
+        mutate(type = 'deaths',
+               date = str_replace(date, '([0-9]+)/([0-9]+)/([0-9]+)', '20\\3-\\1-\\2'),
+               date = anydate(date))
+
+    jh.data.confirmed <- jh.data.raw.confirmed %>%
+        melt(id.vars = c('Province/State', 'Country/Region', 'Lat', 'Long'),
+             variable.name = 'date',
+             value.name = 'cases') %>%
+        tibble ()%>%
+        mutate(type = 'confirmed',
+               date = str_replace(date, '([0-9]+)/([0-9]+)/([0-9]+)', '20\\3-\\1-\\2'),
+               date = anydate(date))
+
+    jh.data <- bind_rows(jh.data.deaths, jh.data.confirmed) %>%
+        select(state = 'Country/Region', date, type, cases) %>%
+        group_by(state, date, type) %>%
+        summarise(cases = sum(cases)) %>%
+        ungroup() %>%
+        mutate(state = jh.convert.names(state)) %>%
+        mutate(state = eu.convert.names(state))
+
+    populations <- run.cache(wb, indicator = "SP.POP.TOTL", show.message = FALSE) %>%
+        group_by(iso3c) %>%
+        filter(date == max(date)) %>%
+        select(population = value, state = country, iso3c) %>%
+        ungroup() %>%
+        #
+        add_row(state = 'Diamond Princess', population = 3711, iso3c = 'Diamond Princess') %>%
+        add_row(state = 'Taiwan', population = 23603121, iso3c = 'TWN') %>%
+        add_row(state = 'Holy See', population = 801, iso3c = 'VAT') %>%
+        add_row(state = 'MS Zaandam', population = 1829, iso3c = 'MS Zaandam') %>%
+        mutate(state = eu.convert.names(state)) %>%
+        mutate(state = jh.convert.names(state)) %>%
+        arrange(state)
+
+    jh.data.pop <- jh.data %>% left_join(populations, by = 'state') %>%
+        group_by(state, type, population) %>%
+        arrange(desc(date)) %>%
+        mutate(cumul = cases) %>%
+        mutate(cases = rollapply(cases, 2, function(ix) { if(length(ix) <= 1) { return(ix) } else { ix[1] - sum(ix[-1]) } }, fill = c(0, 0, 0), align = 'left', partial = TRUE)) %>%
+        select(state, date, type, cases, cumul, population, state.code = iso3c)
+
+    return(list(data = jh.data.pop, source = 'John Hopkins'))
+}
+
+jh.convert.names <- function(state) {
+    convert.tmp <- list(
+        'Brunei' = 'Brunei Darussalam',
+        'Burma' = 'Myanmar',
+        'Cabo Verde' = 'Cape Verde',
+        'Congo (Brazzaville)' = 'Congo',
+        'Congo (Kinshasa)' = 'Democratic Republic of the Congo',
+        'Czechia' = 'Czech Republic',
+        'Guinea-Bissau' = 'Guinea Bissau',
+        'Korea, South' = 'Korea, Rep.',
+        # 'Malawi' = 'Malawi',
+        'Taiwan*' = 'Taiwan',
+        'Tanzania' = 'United Republic of Tanzania',
+        'Timor-Leste' = 'Timor Leste',
+        'United States' = 'USA',
+        'US' = 'USA',
+        'Diamond Princess' = 'Cases on an international conveyance Japan',
+        'West Bank and Gaza' = 'Palestine'
+    )
+
+    state[state %in% names(convert.tmp)] <- sapply(state[state %in% names(convert.tmp)], function(ix) { convert.tmp[[ix]] })
+    return(state)
+}
+
+eu.convert.names <- function(state) {
+    convert.tmp <- eu.convert.names.arr()
+    state[state %in% names(convert.tmp)] <- sapply(state[state %in% names(convert.tmp)], function(ix) { convert.tmp[[ix]] })
+    return(state)
+}
+
+eu.convert.names.arr <- function() {
+    list(
+        '404-1' =  'Anguilla',
+        '404-2' =  'Cases on an international conveyance Japan',
+        '404-2' =  'Guernsey',
+        '404-3' =  'Jersey',
+        '404-4' =  'Montserrat',
+        '404-5' =  'Palestine',
+        #
+        'Bahamas, The' =  'Bahamas',
+        'Cabo Verde' =  'Cape Verde',
+        'Congo, Rep.' =  'Congo',
+        'Cote d\'Ivoire' =  'Cote dIvoire',
+        'Congo, Dem. Rep.' =  'Democratic Republic of the Congo',
+        'Egypt, Arab Rep.' =  'Egypt',
+        'Gambia, The' =  'Gambia',
+        'Guinea-Bissau' =  'Guinea Bissau',
+        'Iran, Islamic Rep.' =  'Iran',
+        'Kyrgyz Republic' =  'Kyrgyzstan',
+        'Lao PDR' =  'Laos',
+        'Russian Federation' =  'Russia',
+        'St. Kitts and Nevis' =  'Saint Kitts and Nevis',
+        'St. Lucia' =  'Saint Lucia',
+        'St. Martin' =  'Saint Vincent and the Grenadines',
+        'Sint Maarten (Dutch part)' =  'Sint Maarten',
+        'Slovak Republic' =  'Slovakia',
+        'Korea, Rep.' =  'South Korea',
+        'Syrian Arab Republic' =  'Syria',
+        'Taiwan*' =  'Taiwan',
+        'Timor-Leste' =  'Timor Leste',
+        'Turks and Caicos Islands' =  'Turks and Caicos islands',
+        'Tanzania' =  'United Republic of Tanzania',
+        'United States' =  'USA',
+        'Virgin Islands (U.S.)' =  'United States Virgin Islands',
+        'Venezuela, RB' =  'Venezuela',
+        #
+        'St. Vincent and the Grenadines' =  'Saint Vincent and the Grenadines'
+    ) %>%
+        return()
+}
+
+
 
 download.eucdc.data <- function() {
     eu.data.raw <- read_csv('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
