65b8bb3b718c8c8e7e9f8cc7c9cc145bdf6b16b6
André Veríssimo <afsverissimo@gmail.com>
2020-04-06 18:19:52 +0200

    removes rolling average for last few days with cases = 0

--- R/download.R
+++ R/download.R
@@ -1,22 +1,32 @@
 fill.missing <- function(dat) {
-    days <- tibble(date = seq.Date(from = min(dat$date %>% unique %>% sort), 
+    days <- tibble::tibble(date = seq.Date(from = min(dat$date %>% unique %>% sort), 
                                    to = max(dat$date %>% unique %>% sort),
                                    by = 'day'),
                    join = 1)
     states <- dat %>% 
-        ungroup() %>% 
-        distinct(state, state.code, population) %>% 
-        mutate(join = 1)
+        dplyr::ungroup() %>% 
+        dplyr::distinct(state, state.code, population) %>% 
+        dplyr::mutate(join = 1)
     
     case.types <- tibble(type = dat$type %>% unique() %>% sort(), join = 1)
     
     index.tbl <- full_join(days, states, by = 'join') %>% 
-        full_join(case.types, by = 'join') %>% 
-        select(-join)
+        dplyr::full_join(case.types, by = 'join') %>% 
+        dplyr::select(-join)
     
-    new.dat <- left_join(index.tbl, dat, by = c('date', 'state', 'state.code', 'type', 'population'))
+    new.dat <- dplyr::left_join(index.tbl, dat, by = c('date', 'state', 'state.code', 'type', 'population'))
     
-    new.dat %>% mutate(cases = if_else(is.na(cases), 0 , cases)) %>% 
+    new.dat %>% 
+        dplyr::mutate(cases = if_else(is.na(cases), 0 , cases)) %>% 
+        dplyr::arrange(date) %>% 
+        dplyr::group_by(state, type) %>% 
+        # build cumul
+        dplyr::mutate(cumul = cumsum(cases)) %>% 
+        # remove latest day from countries that don't have data
+        dplyr::arrange(desc(date)) %>% 
+        dplyr::mutate(rev.cumul = cumsum(cases)) %>% 
+        dplyr::filter(rev.cumul > 0) %>% 
+        dplyr::select(-rev.cumul) %>% 
         return()
 }
 
