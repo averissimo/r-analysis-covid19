94189f877ddcfcb0a35d9ebd1b7a85d245bc8509
André Veríssimo <afsverissimo@gmail.com>
2020-11-06 17:05:12 +0100

    small improvement

--- R/mortality.R
+++ R/mortality.R
@@ -28,8 +28,8 @@ download.evm <- function() {
     geom_crossbar(aes(x = day, y = meanPre2020, ymin=minPre2020, ymax=maxPre2020, fill = 'Min/Max from 2009-19'), alpha = 0, size = .1, width = 1) +
     geom_crossbar(aes(x = day, y = meanPre2020, ymin=minPre2020, ymax=maxPre2020, color = 'Min/Max from 2009-19', fill = 'Min/Max from 2009-19'), alpha = 1, size = .1, width = 1, show.legend = FALSE) +
     #
-    geom_crossbar(aes(x = day, y = meanPre2020, ymin=meanPre2020 - sdPre2020, ymax=meanPre2020 + sdPre2020, fill = 'Error bars from 2009-19'), alpha = 0, size = .1, width = 1) +
-    geom_crossbar(aes(x = day, y = meanPre2020, ymin=meanPre2020 - sdPre2020, ymax=meanPre2020 + sdPre2020, color = 'Error bars from 2009-19', fill = 'Error bars from 2009-19'), alpha = .4, size = .1, width = 1, show.legend = FALSE) +
+    geom_crossbar(aes(x = day, y = meanPre2020, ymin=meanPre2020 - sdPre2020, ymax=meanPre2020 + sdPre2020, fill = '2009-19'), alpha = 0, size = .1, width = 1) +
+    geom_crossbar(aes(x = day, y = meanPre2020, ymin=meanPre2020 - sdPre2020, ymax=meanPre2020 + sdPre2020, color = '2009-19', fill = '2009-19'), alpha = .4, size = .1, width = 1, show.legend = FALSE) +
     #
     geom_smooth(aes(x = day, y = value, color = 'Smooth regression from 2009-19', group = NA), span = 0.1, method = 'loess', formula = y ~ x, na.rm = TRUE, se = FALSE) +
     geom_smooth(aes(x = day, y = meanPre2020, color = 'Smooth regression from 2009-19', group = NA), span = 0.1, method = 'loess', formula = y ~ x, na.rm = TRUE, show.legend = FALSE) +
@@ -40,15 +40,15 @@ download.evm <- function() {
     geom_smooth(aes(x = day, y = value, group = variable, color = 'Smooth regression from Year'), span = 0.1, method = 'loess', formula = y ~ x, na.rm = TRUE, show.legend = FALSE) +
     #
     geom_blank(data = data.frame(x = rep(5, 5), y = 0.5, 
-                                 group = c("Min/Max from 2009-19", "Error bars from 2009-19", "Smooth regression from 2009-19", "Year", "Smooth regression from Year")), 
+                                 group = c("Min/Max from 2009-19", "2009-19", "Smooth regression from 2009-19", "Year", "Smooth regression from Year")), 
                aes(y = y, color = group, fill = group),
                show.legend = FALSE) +
     scale_x_discrete(breaks = function(ix) {
       ix[c(FALSE, TRUE, TRUE, TRUE, TRUE)] <- FALSE
       return(ix)
     }) +
-    scale_color_manual("", values = c("darkgray", "darkgreen", "green", "red", 'darkred'), breaks=c("Min/Max from 2009-19", "Error bars from 2009-19", "Smooth regression from 2009-19", "Year", "Smooth regression from Year")) +
-    scale_fill_manual("Legend_no", values = c("darkgray", "darkgreen", "green", "red", 'darkred'), guide=FALSE, breaks=c("Min/Max from 2009-19", "Error bars from 2009-19", "Smooth regression from 2009-19", "Year", "Smooth regression from Year")) +
+    scale_color_manual("", values = c("darkgreen", "darkgray", "green", "red", 'darkred'), breaks=c("2009-19", "Min/Max from 2009-19", "Smooth regression from 2009-19", "Year", "Smooth regression from Year")) +
+    scale_fill_manual("Legend_no", values = c("darkgreen", "darkgray", "green", "red", 'darkred'), guide=FALSE, breaks=c("2009-19", "Min/Max from 2009-19", "Smooth regression from 2009-19", "Year", "Smooth regression from Year")) +
     facet_wrap( ~ variable, ncol = 1) +
     theme_minimal() +
     
